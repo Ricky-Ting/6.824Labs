b53685e08382fd6c51b175148942fb81070fde5c
Ricky Ting <Ricky.Ting@outlook.com>
2020-01-28 16:53:01 +0800

    Revise PutAppendArgs

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -15,6 +15,8 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Cid   		int
+	RequestId	int
 }
 
 type PutAppendReply struct {
