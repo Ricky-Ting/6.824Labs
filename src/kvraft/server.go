8840fc46f3e28459ceafa8c8482bd965da8ef876
Ricky Ting <Ricky.Ting@outlook.com>
2020-01-29 12:18:17 +0800

    Revise StartKVServer

--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -102,6 +102,7 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 
 	// You may need initialization code here.
 	kv.database = make(map[string]string)
+	kv.applyCond = sync.NewCond(new(sync.Mutex))
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
