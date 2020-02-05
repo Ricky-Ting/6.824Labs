8b69023df8cc51ab3e5017a32a9fea0307513894
Ricky Ting <Ricky.Ting@outlook.com>
2020-02-05 17:19:28 +0800

    Revise some RPC args

--- src/shardmaster/common.go
+++ src/shardmaster/common.go
@@ -36,6 +36,8 @@ type Err string
 
 type JoinArgs struct {
 	Servers map[int][]string // new GID -> servers mappings
+	Cid   		int64
+	RequestId	int
 }
 
 type JoinReply struct {
@@ -45,6 +47,8 @@ type JoinReply struct {
 
 type LeaveArgs struct {
 	GIDs []int
+	Cid   		int64
+	RequestId	int
 }
 
 type LeaveReply struct {
@@ -55,6 +59,8 @@ type LeaveReply struct {
 type MoveArgs struct {
 	Shard int
 	GID   int
+	Cid   		int64
+	RequestId	int
 }
 
 type MoveReply struct {
@@ -64,6 +70,8 @@ type MoveReply struct {
 
 type QueryArgs struct {
 	Num int // desired config number
+	Cid   		int64
+	RequestId	int
 }
 
 type QueryReply struct {
